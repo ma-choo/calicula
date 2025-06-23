6b157bc1a9d17cba1e081a665173d668e260f2b7
ma-choo <imshaunowen@gmail.com>
2025-06-22 20:06:55 -0400

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,10 @@
+#ui.py
+
+import curses
+from utils import zeller, is_leap_year
+from subcalendar import Assignment, Subcalendar
+
+Class UI:
+    def __init__():
+        print("UI initiated")
+      
