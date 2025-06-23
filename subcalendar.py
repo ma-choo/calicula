f98b3d2ae14d680fda7a3db034381f3cdb597e50
ma-choo <imshaunowen@gmail.com>
2025-06-22 20:11:28 -0400

    Update subcalendar.py

--- subcalendar.py
+++ subcalendar.py
@@ -1,17 +1,27 @@
 # subcalendar.py
 
 class Assignment:
-    def __init__(self, name: str, day: int, month: int, year: int):
-        self.name = name
-        self.day = day
-        self.month = month
-        self.year = year
+  def __init__(self, name: str, date: str, completed: bool):
+    self.name = name
+    self.completed = completed
+    self.date = date
 
-class Calendar:
-    def __init__(self, name: str):
-        self.name = name
-        self.assignments: List[Assignment] = []
-        self.hidden = False
+  def toggle_completion(self):
+    self.completed = not self.completed
 
-    def add_event(self, assignment: Assignment):
-        self.assignments.append(assignment)
+# create a constructor that takes a filename string
+
+class Subcalendar:
+  def __init__(self, name: str):
+    self.name = name
+    self.color = 0
+    self.assignments = []
+    self.hidden = False
+
+  def add_assignment(self, assignment: Assignment):
+    self.assignments.append(assignment)
+
+  def show(self):
+    print(self.name, self.color, self.hidden)
+    for assignment in self.assignments:
+      print(assignment.name, assignment.date, assignment.completed)
