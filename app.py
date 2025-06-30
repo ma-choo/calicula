e80270eeb19295b913dae1e0333519cbde29feec
ma-choo <imshaunowen@gmail.com>
2025-06-29 20:28:07 -0400

    Fix deployement

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 # app.py
 
-from flask import Flask, Response
-from subcalendar import Subcalendar
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def debug_calendars():
-    subcalendars = Subcalendar.read_all()
-    output = "\n".join(repr(cal) for cal in subcalendars)
-    return Response(output, mimetype="text/plain")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def hello()
+    return "testing cal deployment"
