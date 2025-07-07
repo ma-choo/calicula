1280d12a4f18f9f2e9be2739042f0abfc9ec2cc9
ma-choo <imshaunowen@gmail.com>
2025-07-07 10:38:11 -0400

    flask app

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-# app.py
+"""
+app.py - flask rest api
 
-from flask import Flask
+local usage:
+get:
+curl http://localhost:5000/subcalendars
+
+post:
+curl -X POST http://localhost:5000/subcalendars \
+     -H "Content-Type: application/json" \
+     -d '{"name": "<date>", "assignments": []}'
+
+azure app service deployment environment variables:
+CALICULA_STORAGE_BACKEND -> azure
+AZURE_CONNECTION_STRING
+AZURE_CONTAINER
+"""
+
+from flask import Flask, jsonify, request
+from storage import get_backend
+from subcalendar import Subcalendar
+
 app = Flask(__name__)
+storage = get_backend()
 
-@app.route('/')
-def hello()
-    return "testing cal deployment"
+@app.route("/subcalendars", methods=["GET"])
+def get():
+    subcals = storage.read_all()
+    return jsonify([sc.to_dict() for sc in subcals])
+
+@app.route("/subcalendars", methods=["POST"])
+def post():
+    sc = Subcalendar.from_dict(request.json)
+    storage.write(sc)
+    return jsonify({"status": "ok"}), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
